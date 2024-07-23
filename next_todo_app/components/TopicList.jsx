53e8f35b94baca62b39db0bb1ad350388f32da92
CoolShivas <shivas2710cool00@gmail.com>
2024-07-23 13:50:21 +0530

    Added some CSS in TopicList.

--- next_todo_app/components/TopicList.jsx
+++ next_todo_app/components/TopicList.jsx
@@ -5,13 +5,13 @@ import RemoveBtn from "./RemoveBtn";
 const TopicList = () => {
   return (
     <>
-    <div>
+    <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
         <div>
-            <h2>Topic Title</h2>
+            <h2 className="font-bold text-2xl">Topic Title</h2>
             <div>Topic Description</div>
         </div>
 
-        <div>
+        <div className="flex gap-2">
             <RemoveBtn></RemoveBtn>
             <Link href={"/editTopic/0123"}>
             <HiPencilAlt size={24}/>
